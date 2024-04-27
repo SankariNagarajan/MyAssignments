dd5d89e666083c5300d6ec2d2264e695e8aa0973
SankariNagarajan <168295997+SankariNagarajan@users.noreply.github.com>
2024-04-28 00:15:55 +0530

    Add files via upload

---
+++ myAssignments/day1/HelloTesting.java
@@ -0,0 +1,11 @@
+package myAssignments.day1;
+
+public class HelloTesting {
+	
+	public static void main (String args []) {
+	
+	System.out.println ("Welcome to Testing");
+	
+	}
+
+}
---
+++ myAssignments/day1/SwitchCase.java
@@ -0,0 +1,40 @@
+package myAssignments.day1;
+
+public class SwitchCase {
+	
+	public static void main (String args[]) {
+		
+	 char city = 'C';
+		
+	  switch (city) {
+		
+		case 'C':
+			
+			System.out.println("Chennai city");
+			break;
+			
+		case 'H':
+			
+			System.out.println("Hyderabad city");
+			break;
+			
+		case 'B':
+			
+			System.out.println("Bangalore city");
+			break;
+			
+		case 'M':
+			
+			System.out.println("Mumbai city");
+			break;
+			
+		case 'D':
+			
+			System.out.println("Delhi city");
+			break;
+		
+		}
+		
+	}
+
+}
